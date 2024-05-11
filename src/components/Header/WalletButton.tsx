84d8733ed0120244d69a719b9ae67c1a1a5f4d1b
oynozan <oynozan@hotmail.com>
2024-05-11 22:54:00 +0300

    basic components

---
+++ src/components/Header/WalletButton.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Button from "../Button"
+
+export default function WalletButton() {
+    return (
+        <Button type="dark">
+            Connect Wallet
+        </Button>
+    )
+}
---
+++ src/lib/states.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+
+/* Modal States */
+// Wallet Modal
+interface ModalStore {
+    modal: string;
+    options: any;
+    loading: boolean;
+    setModal: (type: string, options: any) => void;
+    setLoading: (loading: boolean) => void;
+}
+
+export const useModalStore = create<ModalStore>((set) => ({
+    modal: "", // Modal Key
+    options: {},
+    loading: false,
+    setModal: (type, options = {}) => set(() => ({
+        modal: type,
+        options: options,
+    })),
+    setLoading: (loading) => set(() => ({ loading }))
+}));
