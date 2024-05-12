88acad604b1039583cf94179e2610b112a575b44
nickmura <nickmuradev@gmail.com>
2024-05-12 02:28:49 -0600

    works but has a hydration error

--- src/components/Header/WalletButton.tsx
+++ src/components/Header/WalletButton.tsx
@@ -2,10 +2,20 @@
 
 import Button from "../Button"
 
+
+import { ConnectButton, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui.js/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+const networks = {
+	localnet: { url: getFullnodeUrl('localnet') },
+	devnet: { url: getFullnodeUrl('devnet') },
+	testnet: { url: getFullnodeUrl('testnet') },
+	mainnet: { url: getFullnodeUrl('mainnet') },
+};
 export default function WalletButton() {
     return (
-        <Button type="dark">
-            Connect Wallet
-        </Button>
+        <ConnectButton />
     )
 }
