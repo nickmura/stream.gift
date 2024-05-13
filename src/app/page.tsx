f28d6422ee08b37cc77b12e6d73ffe0c095d6ae2
nickmura <nickmuradev@gmail.com>
2024-05-12 23:31:05 -0600

    added activity component on page kinda dumb commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Activity from "@/components/Activity";
 import Search from "@/components/Search";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
             className="flex items-center justify-center py-20"
         >
             <Search />
+            <Activity/>
         </div>
     )
 }
