e498a936c3f8a57a877e618c66f0779fb0c8a928
nickmura <nickmuradev@gmail.com>
2024-05-20 23:40:19 -0600

    added PTB call

--- src/components/Header/WalletButton.tsx
+++ src/components/Header/WalletButton.tsx
@@ -3,9 +3,11 @@
 import Button from "../Button"
 
 
-import { ConnectButton, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { ConnectButton,  SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
 import { getFullnodeUrl } from '@mysten/sui.js/client';
+import { TransactionBlock } from "@mysten/sui.js/transactions";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import input from "postcss/lib/input";
 
 const queryClient = new QueryClient();
 const networks = {
@@ -14,8 +16,22 @@ const networks = {
 	testnet: { url: getFullnodeUrl('testnet') },
 	mainnet: { url: getFullnodeUrl('mainnet') },
 };
+
+async function callPTB() {
+
+	const txb = new TransactionBlock();
+	const [coin] = txb.splitCoins(txb.gas, [txb.pure(100), txb.pure(200)])
+	txb.transferObjects([coin], txb.pure(`0x792423f7950d75fa476fd618bc0c647ce1183ceab19059dd00bdf5690e01db78`));
+	let s = txb.serialize()
+
+}   
+
 export default function WalletButton() {
     return (
-        <ConnectButton />
+		<>
+			<button onClick={callPTB} className=''>test</button>
+        	<ConnectButton />
+		</>
+
     )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import WalletButton from "./WalletButton";
 
+import PTB from "./PTB";
+
+
 export default function Header() {
     return (
         <header
@@ -7,6 +10,7 @@ export default function Header() {
                 flex items-center justify-end
                 border-b-2 border-white px-5 h-24"
         >
+            <PTB/>
             <WalletButton />
         </header>
     )
